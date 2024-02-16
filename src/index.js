289e458e6209570fdb83f2d845b51c34066b8085
singhab <abhisheksingh@tabcorp.com.au>
2024-02-16 15:00:25 +0530

    add different colors

--- src/index.js
+++ src/index.js
@@ -4,108 +4,47 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import './calender.scss';
+import {data} from "./processData";
 
 const localizer = momentLocalizer(moment);
 
 function App() {
-    const data = [
-        {
-            trackName: "Olive track",
-            reviewStatus: "YET_TO_START",
-            assessments: [
-                {
-                    id: "65c20d2ad56ce0e86945139d",
-                    name: "javascript",
-                    startDate: "2024-02-18T10:42:50.637Z",
-                    endDate: "2024-02-20T10:43:52.447Z",
-                    status: "COMPLETED"
-                }
-            ],
-            id: "65c20d0ed56ce0e86945138d",
-            projectName: "Olive Gaea",
-            accountName: "Olive Gaea India Private Limited",
-            startDate: "2024-02-18T10:42:22.432Z",
-            endDate: "2024-02-20T10:43:52.518Z"
-        },
-        {
-            trackName: "Neo track",
-            reviewStatus: "IN_PROGRESS",
-            assessments: [
-                {
-                    id: "65c20d2ad56ce0e8694532i5",
-                    name: "javascript",
-                    startDate: "2024-02-21T10:42:50.637Z",
-                    endDate: "2024-02-24T10:43:52.447Z",
-                    status: "COMPLETED"
-                }
-            ],
-            id: "65c20d0ed56ce0e86945432i5",
-            projectName: "Gi Goe",
-            accountName: "Gi Goe India Pvt Ltd",
-            startDate: "2024-02-21T10:42:22.432Z"
-        },
-        {
-            trackName: "TTN track",
-            reviewStatus: "COMPLETED",
-            assessments: [
-                {
-                    id: "65c20d2ad56ce0e8694532i8",
-                    name: "javascript",
-                    startDate: "2024-02-21T10:42:50.637Z",
-                    endDate: "2024-02-24T10:43:52.447Z",
-                    status: "COMPLETED"
-                }
-            ],
-            id: "65c20d0ed56ce0e86945432i8",
-            projectName: "TTN World",
-            accountName: "TTN India Pvt Ltd",
-            startDate: "2024-02-21T10:42:22.432Z",
-            endDate: "2024-02-24T10:43:52.518Z"
-        },
-        {
-            trackName: "MLI track",
-            reviewStatus: "NO_STATUS",
-            assessments: [
-                {
-                    id: "65c20d2ad56ce0e8694532i7",
-                    name: "javascript",
-                    startDate: "2024-02-26T10:42:50.637Z",
-                    endDate: "2024-02-27T10:43:52.447Z",
-                    status: "NO_STATUS"
-                }
-            ],
-            id: "65c20d0ed56ce0e86945432i8",
-            projectName: "MLI World",
-            accountName: "MLI India Pvt Ltd",
-            startDate: "2024-02-26T10:42:22.432Z",
-            endDate: "2024-02-27T10:43:52.518Z"
-        }
-    ];
+    function filterMap (status) {
+        return data.filter(item => item.reviewStatus === status).map((item, index) => {
+            item.dataClass = `${item.reviewStatus}-${index}`;
+            return item;
+        });
+    }
+    const yetToStartResult = filterMap('YET_TO_START');
+    const inProgressResult = filterMap('IN_PROGRESS');
+    const noStatusResult = filterMap('NO_STATUS');
+    const completedResult = filterMap('COMPLETED');
+    const dataResult = yetToStartResult.concat(inProgressResult, noStatusResult, completedResult);
 
-    const eventResult = data.map(item => {
+    const eventResult = dataResult.map((item, index) => {
             if(!item.endDate){
                 item.reviewStatus = 'IN_PROGRESS';
                 item.endDate = item.startDate;
             }
             return {
                 title: item.projectName+' - '+item.trackName+' - '+item.reviewStatus,
-                start: item.startDate,
-                end: item.endDate,
-                description: item.reviewStatus
+                start: new Date(item.startDate),
+                end: new Date(item.endDate),
+                description: item.dataClass
                 //resource: 'test'
             }
         }
     );
     
   const event = ({ event }) => {
-    console.log(event);
+    //console.log(event);
     return (
       <div className={event.description.toLowerCase()}> {event.title}</div>
     );
   };
 
   return (
-    <div className="App" style={{ minHeight: 580 }}>
+    <div className="App" style={{ minHeight: 580}}>
         
         <Calendar
             localizer={localizer}
